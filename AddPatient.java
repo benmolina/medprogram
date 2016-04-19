48ea6411587db1ed80cb7b02ad8b4ce12297c29a
benmolina <bmolina@email.uark.edu>
2016-04-19 18:48:15 -0500

    Errors fixed and ready for implementation
    Per Zach, AddPatient was looked into and errors have been fixed, plus a minor graphical change (changed Date of Birth to calendar, rather than textbox).

---
+++ AddPatient.java
@@ -0,0 +1,277 @@
+package medProgram;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import com.toedter.calendar.JDateChooser;
+
+public class AddPatient extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private JPanel contentPane;
+	private JTextField txtFirstName;
+	private JTextField txtLastName;
+	private JTextField txtstreet;
+	private JTextField txtcity;
+	private JTextField txtstate;
+	private JTextField txtzipcode;
+	private JTextField txtphonenumber;
+	private JTextField txtgender;
+	
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		new AddPatient("");
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public AddPatient(String username) {
+		
+		setTitle("Add Patient");
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 316, 562);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblFirstName = new JLabel("First Name:");
+		lblFirstName.setBounds(6, 17, 73, 16);
+		contentPane.add(lblFirstName);
+		
+		txtFirstName = new JTextField();
+		txtFirstName.setBounds(129, 11, 134, 28);
+		contentPane.add(txtFirstName);
+		txtFirstName.setColumns(10);
+		
+		JLabel lblLastName = new JLabel("Last Name:");
+		lblLastName.setBounds(6, 57, 73, 16);
+		contentPane.add(lblLastName);
+		
+		txtLastName = new JTextField();
+		txtLastName.setBounds(129, 51, 134, 28);
+		contentPane.add(txtLastName);
+		txtLastName.setColumns(10);
+		
+		JLabel lbldateofbirth = new JLabel("Date of Birth:");
+		lbldateofbirth.setBounds(6, 102, 97, 16);
+		contentPane.add(lbldateofbirth);
+		
+		JLabel lblRole = new JLabel("Gender:");
+		lblRole.setBounds(6, 141, 61, 16);
+		contentPane.add(lblRole);
+		
+		JLabel lblPassword = new JLabel("Street:");
+		lblPassword.setBounds(6, 177, 61, 16);
+		contentPane.add(lblPassword);
+		
+		JLabel lblConfirm = new JLabel("City:");
+		lblConfirm.setBounds(6, 217, 61, 16);
+		contentPane.add(lblConfirm);
+
+		JButton btnAddPatient = new JButton("Add Patient");
+		btnAddPatient.setBounds(6, 483, 117, 29);
+		contentPane.add(btnAddPatient);
+		
+		txtstreet = new JTextField();
+		txtstreet.setBounds(129, 171, 134, 29);
+		contentPane.add(txtstreet);
+		txtstreet.setColumns(10);
+		
+		txtcity = new JTextField();
+		txtcity.setBounds(129, 211, 134, 28);
+		contentPane.add(txtcity);
+		txtcity.setColumns(10);
+		
+		txtstate = new JTextField();
+		txtstate.setBounds(129, 254, 134, 28);
+		contentPane.add(txtstate);
+		txtstate.setColumns(10);
+		
+		JLabel lblState = new JLabel("State:");
+		lblState.setBounds(6, 261, 46, 14);
+		contentPane.add(lblState);
+		
+		JLabel lblZipcode = new JLabel("Zipcode:");
+		lblZipcode.setBounds(6, 304, 73, 14);
+		contentPane.add(lblZipcode);
+		
+		JLabel lblPhonenumber = new JLabel("Phone number:");
+		lblPhonenumber.setBounds(6, 347, 97, 14);
+		contentPane.add(lblPhonenumber);
+		
+		txtzipcode = new JTextField();
+		txtzipcode.setBounds(129, 297, 134, 28);
+		contentPane.add(txtzipcode);
+		txtzipcode.setColumns(10);
+		
+		txtphonenumber = new JTextField();
+		txtphonenumber.setBounds(127, 340, 136, 28);
+		contentPane.add(txtphonenumber);
+		
+		txtgender = new JTextField();
+		txtgender.setBounds(129, 135, 134, 29);
+		contentPane.add(txtgender);
+		txtgender.setColumns(10);
+		
+		JButton btnCancel = new JButton("Cancel");
+		btnCancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int dialogBtn = JOptionPane.YES_NO_OPTION;
+				int dialogResult = JOptionPane.showConfirmDialog(null, "Are you sure you want to cancel?", "Warning", dialogBtn);
+				if (dialogResult == 0)
+					{
+						dispose();
+					}
+			}
+		});
+		btnCancel.setBounds(157, 483, 117, 29);
+		contentPane.add(btnCancel);
+		
+		JDateChooser dtcDOB = new JDateChooser();
+		dtcDOB.setBounds(129, 95, 130, 28);
+		contentPane.add(dtcDOB);
+		
+		
+		this.setLocationRelativeTo(this);
+		setVisible(true);
+	
+		btnAddPatient.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				try {
+					Connection conn=DriverManager.getConnection("jdbc:mysql://99.98.84.144:3306/medprogram", "root", "medProgram");
+					Statement stmt = conn.createStatement();
+					
+					String firstName, lastName, gender, street, city, state, zipcode, phonenumber, finalDOB;
+					Date dateofbirth;
+					
+					//Form checking
+					if(txtFirstName.getText().equals("")) {
+						JOptionPane.showMessageDialog(null,"First name cannot be empty");
+						return;
+					}
+					else {
+						firstName = txtFirstName.getText();
+					}
+					
+					if(txtLastName.getText().equals("")) {
+						JOptionPane.showMessageDialog(null,"Last name cannot be empty");
+						return;
+					}
+					else {
+						lastName = txtLastName.getText();						
+					}
+					
+					if(dtcDOB.getDate().equals("")) {
+						JOptionPane.showMessageDialog(null,"Date of Birth cannot be empty");
+						return;
+					}
+					else {
+						java.util.Date birth = dtcDOB.getDate();
+						dateofbirth = new Date(birth.getTime());
+						Calendar conversion = Calendar.getInstance();
+						conversion.setTime(dateofbirth);
+						java.util.Date convertedDate = conversion.getTime();
+						
+						java.text.SimpleDateFormat sdf = 
+							     new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm");
+
+						finalDOB = sdf.format(convertedDate);
+						System.out.print(finalDOB);
+					}
+					
+					if(txtstreet.getText().equals("")) {
+						JOptionPane.showMessageDialog(null,"Street cannot be empty");
+						return;
+					}
+					else {
+						street = txtstreet.getText();						
+					}
+					
+					if(txtcity.getText().equals("")) {
+						JOptionPane.showMessageDialog(null,"City cannot be empty");
+						return;
+					}
+					else {
+						city = txtcity.getText();						
+					}
+					
+					if(txtstate.getText().equals("")) {
+						JOptionPane.showMessageDialog(null,"Last name cannot be empty");
+						return;
+					}
+					else {
+						state = txtstate.getText();						
+					}
+					
+					if(txtzipcode.getText().equals("")) {
+						JOptionPane.showMessageDialog(null,"Last name cannot be empty");
+						return;
+					}
+					else {
+						zipcode = txtzipcode.getText();						
+					}
+					
+					if(txtphonenumber.getText().equals("")) {
+						JOptionPane.showMessageDialog(null,"Last name cannot be empty");
+						return;
+					}
+					else {
+						phonenumber = txtphonenumber.getText();						
+					}
+					
+					if(txtgender.getText().equals("")) {
+						JOptionPane.showMessageDialog(null,"Last name cannot be empty");
+						return;
+					}
+					else {
+						gender = txtgender.getText();						
+					}
+					
+					//end of form checking
+					
+					
+					stmt.executeUpdate("Insert into patientheader (firstname, lastname, dateofbirth, gender, street, city, state, zipcode, phonenumber)" 
+							+ "Values ('" + firstName + "', '" + lastName + "', '" + finalDOB + "', '" + gender + "', '" + street + "', '" + city + "', '" + state + "', '" + zipcode + "', '" + 
+							phonenumber + "');");
+					
+					stmt.closeOnCompletion();
+					
+					conn.close();
+					
+					JOptionPane.showMessageDialog(null,"Patient " + firstName + " " + lastName + " has been added. ");
+					
+					dispose();
+					
+				} catch (SQLException e1) {
+					JOptionPane.showMessageDialog(null,"There was an error adding this Patient.");
+					e1.printStackTrace();
+				}
+				
+			}
+		});
+		
+	}
+}
